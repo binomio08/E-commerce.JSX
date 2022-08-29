bf3a77c24b29ebb933293868ab4e82376cb5def7
Leandro Troncoso <binomio08@hotmail.com>
2022-08-28 22:24:47 -0300

    firts commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import '../../App.css'
+import Mando from "../assets/mando.ico"
+
+const navbar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg bg-black navbar-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src={Mando} alt="" />
+                    </a>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Productos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link " href="#" >Nosotros</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link " href="#" >Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default navbar;

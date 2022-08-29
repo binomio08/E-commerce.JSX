e2b691d54a8006f9c30f7ad97e84102e8ea33fc6
Leandro Troncoso <binomio08@hotmail.com>
2022-08-29 20:19:40 -0300

    arreglo className

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,13 +5,13 @@ import Mando from "../assets/mando.ico"
 const navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-black navbar-dark">
+            <nav className="navbar navbar-expand-lg bg-black navbar-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
                         <img src={Mando} alt="" />
                     </a>
                     <button
-                        class="navbar-toggler"
+                        className="navbar-toggler"
                         type="button"
                         data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent"
@@ -19,7 +19,7 @@ const navbar = () => {
                         aria-expanded="false"
                         aria-label="Toggle navigation"
                     >
-                        <span class="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
